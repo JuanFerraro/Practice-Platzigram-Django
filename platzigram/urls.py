0feb477cef96fee157578177f1b392147ee72c1e
Juan Ferraro <juansebastianferraro@gmail.com>
2023-07-24 18:47:47 -0500

    Created post model and post admin 📚

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,5 +1,7 @@
 # Django
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 # Platzigram
@@ -18,4 +20,5 @@ urlpatterns = [
 
     # Posts routes
     path('posts/', posts_views.list_posts),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
